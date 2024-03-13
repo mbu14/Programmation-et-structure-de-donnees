fd0a45a4077b35901de69a1bef2b89646ed8e808
mbu14 <163116014+mbu14@users.noreply.github.com>
2024-03-13 16:38:06 +0100

    Exercice1et2

---
+++ TD1/Exercice1et2.py
@@ -0,0 +1,16 @@
+tirage = ['a', 'r', 'b', 'g', 'e', 's', 'c', 'j']
+mots_possibles = ['sacre', 'sabre', 'baser', 'cabre', 'garce', 'crase', 'brase', 'barge', 'caser', 'jaser', 'crabe', 'scare', 'aber', 'gare', 'sage', 'gars', 'rase', 'arec', 'acre', 'jars', 'case', 'base', 'cage', 'rage', 'jase', 'bras', 'race', 'ars', 'sac', 'arc', 'are', 'jar', 'jas', 'bar', 'bas', 'ace', 'cas', 'car', 'age', 'bac', 'cab', 'as', 'ra', 'sa', 'a']
+
+def motlepluslong(tirage,mots_possibles):
+    #tirage
+    #mots_possibles = ['sacre', 'sabre', 'baser', 'cabre', 'garce', 'crase', 'brase', 'barge', 'caser', 'jaser', 'crabe', 'scare', 'aber', 'gare', 'sage', 'gars', 'rase', 'arec', 'acre', 'jars', 'case', 'base', 'cage', 'rage', 'jase', 'bras', 'race', 'ars', 'sac', 'arc', 'are', 'jar', 'jas', 'bar', 'bas', 'ace', 'cas', 'car', 'age', 'bac', 'cab', 'as', 'ra', 'sa', 'a']
+    mot=mots_possibles[0]
+    n=len(mots_possibles)
+    for i in range (1,n):
+        if len (mots_possibles[i]) > len(mot):
+            for lettres in mots_possibles[i]:
+                if lettres in tirage:
+                        mot=mots_possibles[i]
+    return mot
+
+print(motlepluslong(tirage,mots_possibles))
