49e0abf4e6a9ae855d59e3fe9fadbb227ffc6bbe
mbu14 <163116014+mbu14@users.noreply.github.com>
2024-03-16 14:21:05 +0100

    Exercice3

--- TD1/Exercice1et2.py
+++ TD1/Exercice1et2.py
@@ -2,8 +2,6 @@ tirage = ['a', 'r', 'b', 'g', 'e', 's', 'c', 'j']
 mots_possibles = ['sacre', 'sabre', 'baser', 'cabre', 'garce', 'crase', 'brase', 'barge', 'caser', 'jaser', 'crabe', 'scare', 'aber', 'gare', 'sage', 'gars', 'rase', 'arec', 'acre', 'jars', 'case', 'base', 'cage', 'rage', 'jase', 'bras', 'race', 'ars', 'sac', 'arc', 'are', 'jar', 'jas', 'bar', 'bas', 'ace', 'cas', 'car', 'age', 'bac', 'cab', 'as', 'ra', 'sa', 'a']
 
 def motlepluslong(tirage,mots_possibles):
-    #tirage
-    #mots_possibles = ['sacre', 'sabre', 'baser', 'cabre', 'garce', 'crase', 'brase', 'barge', 'caser', 'jaser', 'crabe', 'scare', 'aber', 'gare', 'sage', 'gars', 'rase', 'arec', 'acre', 'jars', 'case', 'base', 'cage', 'rage', 'jase', 'bras', 'race', 'ars', 'sac', 'arc', 'are', 'jar', 'jas', 'bar', 'bas', 'ace', 'cas', 'car', 'age', 'bac', 'cab', 'as', 'ra', 'sa', 'a']
     mot=mots_possibles[0]
     n=len(mots_possibles)
     for i in range (1,n):
@@ -13,4 +11,12 @@ def motlepluslong(tirage,mots_possibles):
                         mot=mots_possibles[i]
     return mot
 
-print(motlepluslong(tirage,mots_possibles))
+#print(motlepluslong(tirage,mots_possibles))
+
+available_words=[]
+file= open("frenchssaccent.dic",'r')
+for ligne in file:
+     available_words.append(ligne[0:len(ligne)-1])
+file.close()
+
+print(motlepluslong(tirage,file))
