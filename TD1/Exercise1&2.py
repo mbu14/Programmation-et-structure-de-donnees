1c1fe68f4eb9aabc3e88c8f10531dcba76098d98
mbu14 <163116014+mbu14@users.noreply.github.com>
2024-03-17 11:02:33 +0100

    version 2

---
+++ TD1/Exercise1&2.py
@@ -0,0 +1,25 @@
+#Exercise 1
+draw = ['a', 'r', 'b', 'g', 'e', 's', 'c', 'j']
+possible_words = ['sacre', 'sabre', 'baser', 'cabre', 'garce', 'crase', 'brase', 'barge', 'caser', 'jaser', 'crabe', 'scare', 'aber', 'gare', 'sage', 'gars', 'rase', 'arec', 'acre', 'jars', 'case', 'base', 'cage', 'rage', 'jase', 'bras', 'race', 'ars', 'sac', 'arc', 'are', 'jar', 'jas', 'bar', 'bas', 'ace', 'cas', 'car', 'age', 'bac', 'cab', 'as', 'ra', 'sa', 'a']
+
+def longest_word(draw,possible_words):
+    word=possible_words[0]
+    n=len(possible_words)
+    for i in range (1,n):
+        if len (possible_words[i]) > len(word):
+            for letters in possible_words[i]:
+                if letters in draw:
+                        word=possible_words[i]
+    return word
+
+#print(longest_word(draw,possible_words))
+
+#Exercise 2
+available_words=[]
+file= open('mots.sansaccent.txt','r')
+for ligne in file:
+     available_words.append(ligne[0:len(ligne)-1])
+file.close()
+
+#print (available_words)
+#print(longest_word(draw,available_words))
